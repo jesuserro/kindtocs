ed84c4890a6abc59ae988208e109c66aab1db17a
olagato <olagato@gmail.com>
2022-03-31 09:15:55 +0200

    test(Console): Revoved console.log

--- src/kindtocs/global.ts
+++ src/kindtocs/global.ts
@@ -100,7 +100,6 @@ export function getIsFavorite (note: string): string {
 }
 
 export function getHeader (note: string): string {
-  console.log("note", note);
   const headers = note.match(/\.(h[0-9]{1})/);
   if(note && headers && headers.length > 0){
     return headers[1];
