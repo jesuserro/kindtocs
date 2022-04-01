e59ccc17618fb35924b5b9ee082597bfc33b44ef
olagato <olagato@gmail.com>
2022-04-01 09:11:41 +0200

    refactor(Highlight.newLine): Se pone en un único if

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -198,15 +198,13 @@ export class Renderer {
     if(header == ""){
       userTemplate = this.notesSinHeaderTpl();
       highlight.tab = "\n\n\r";
+      highlight.newLine = "\n\r";
     }
 
     highlight.icon = getColorIcon(highlight.color);
     highlight.isFavorite = getIsFavorite(note);
     highlight.ref = getRef(note);
     highlight.noteText = getNoteText(note);
-    if(header == ""){
-      highlight.newLine = "\n\r";
-    }
 
     const highlightParams = { ...highlight, appLink: appLink(book, highlight) };
     const highlightTemplate = highlightTemplateWrapper.replace('{{ content }}', userTemplate);
